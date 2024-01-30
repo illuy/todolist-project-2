3c24c71719a3b4ce771d3288cc6c50a3d8fbf522
illu_y <ii_2002@naver.com>
2024-01-30 17:43:10 +0900

    header style-components로 생성

--- src/components/LayoutHeader.jsx
+++ src/components/LayoutHeader.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Tilte = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
 
 export const LayoutHeader = () => {
-  return (
-    <div>layoutHeader</div>
-  )
-}
+    return (
+        <>
+            <Tilte>
+                <h1>todolist</h1>
+                <p>react</p>
+            </Tilte>
+        </>
+    );
+};
